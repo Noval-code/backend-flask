465d57d1f8d8b2b197cd53bb8fb18b8f15957941
Muhammad Noval Aula <117803258+Noval-code@users.noreply.github.com>
2025-06-28 02:10:20 +0700

    setting config

--- api/config.py
+++ api/config.py
@@ -2,9 +2,8 @@ import os
 from datetime import timedelta
 
 class Config:
-    GOOGLE_CLIENT_ID = "582378404984-ckh17s4d1jut74338h4drf8h8186bqbp.apps.googleusercontent.com"
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    MONGO_URI = 'mongodb+srv://balletmotion30:Password30@capstone.a6u5hbn.mongodb.net/ballet_motion?retryWrites=true&w=majority&appName=Capstone'
+    MONGO_URI = os.environ.get('MONGO_URI')
     MONGO_DBNAME = 'ballet_motion'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "jwt_secret_key"
@@ -13,9 +12,10 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = 'elcapstone69@gmail.com'
-    MAIL_PASSWORD = 'siad fffv ajwx zsly'
-    MAIL_DEFAULT_SENDER = 'elcapstone69@gmail.com'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     BASE_URL = 'http://localhost:5000'
     OTP_EXPIRY_MINUTES = 10
 
